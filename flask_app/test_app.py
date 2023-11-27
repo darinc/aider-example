1a25cb169d3d191d390ece249b644c6c4d7713d8
Darin Chambers <darin@1112.net>
2023-11-26 21:02:30 -0500

    Refactored test_factorial_endpoint_error method to remove redundant test case.

--- flask_app/test_app.py
+++ flask_app/test_app.py
@@ -63,11 +63,5 @@ class FlaskAppTestCase(unittest.TestCase):
             expected_result *= i
         self.assertEqual(response.json['result'], expected_result)
 
-    def test_factorial_endpoint_error(self):
-        n = -5
-        response = self.client.get(f'/factorial/{n}')
-        self.assertEqual(response.status_code, 400, f"Expected status code 400, got {response.status_code}")
-        self.assertEqual(response.json.get('error'), 'Invalid input: n must be non-negative', f"Expected error message 'Invalid input: n must be non-negative', got {response.json.get('error')}")
-
 if __name__ == '__main__':
     unittest.main()
